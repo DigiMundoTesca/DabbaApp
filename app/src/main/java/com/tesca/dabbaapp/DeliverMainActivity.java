787a58d7022b8de1bc32b8b3b60dc26d78ce30d6
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-12-12 11:55:31 -0600

    Cuadro de dialogo al cerrar la aplicaci�on

--- app/src/main/java/com/tesca/dabbaapp/DeliverMainActivity.java
+++ app/src/main/java/com/tesca/dabbaapp/DeliverMainActivity.java
@@ -1,6 +1,8 @@
 package com.tesca.dabbaapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,5 +22,25 @@ public class DeliverMainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("¿Salir de la aplicación?")
+                .setTitle("Alerta")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog dialog1 = dialog.create();
+        dialog1.show();
     }
 }
