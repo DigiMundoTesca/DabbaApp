257ae4a871447a2cbf32e3f530f0552de8ccf64e
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2017-01-02 15:34:44 -0600

    Error de alerta corregido

--- app/src/main/java/com/tesca/dabbaapp/AlertFragment.java
+++ app/src/main/java/com/tesca/dabbaapp/AlertFragment.java
@@ -26,23 +26,11 @@ public class AlertFragment extends DialogFragment {
     }
 
    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
        View view = inflater.inflate(R.layout.fragment_alert, container);
-
        getDialog().setTitle("Alerta!!!");
-
-        /*final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(R.layout.fragment_alert);
-        builder.setTitle(R.string.alert_tittle);
-        builder.setCancelable(true);
-        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-
-                    }
-                });*/
 
         // Inflate the layout for this fragment
         return view;
-                //builder.create();
     }
 }
